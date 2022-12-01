052674d23d83f3c4dc54c188904c0c901e9b45c0
Prilly <prilly@DOKUs-MacBook-Pro-4.local>
2022-12-01 21:37:07 +0700

    update name function and add event section to index

--- components/SecEvent.tsx
+++ components/SecEvent.tsx
@@ -1,14 +1,14 @@
 import Script from 'next/script';
 
-export default function InstagramEmbeded() {
+export default function SecEvent() {
   return (
     <section className="mt-10 relative" id="event">
       <div className="container mx-auto justify-center text-center flex flex-wrap">
         <h2 className="font-bold text-4xl font-mulish">EVENT</h2>
       </div>
-      <div className="h-screen bg-white">
+      <div className="-mb-10 h-screen bg-white">
         <Script src="https://embedsocial.com/js/iframe.js"></Script>
-        <iframe className="rounded-xl w-full h-full" scrolling="no" src="https://embedsocial.com/api/pro_hashtag/32e6f83904d726ad3d5ae2208ac825ba3bb52fbf"></iframe>
+        <iframe className="rounded-xl w-full h-full mb-100" scrolling="no" src="https://embedsocial.com/api/pro_hashtag/32e6f83904d726ad3d5ae2208ac825ba3bb52fbf"></iframe>
         <script>iFrameResize();</script>
       </div>
     </section>
